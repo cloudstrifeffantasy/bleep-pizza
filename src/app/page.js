f64b7461dbb9b830d2b5f5a9fd2028b78924cd12
Justin Salanga <justin.salanga@gmail.com>
2026-05-06 00:10:13 -0700

    Tracking Test

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,12 @@
 
 export default function Home() {
   return (
-      <main className="container mx-auto px-4">
-        <h1 className="text-4xl font-bold text-center my-8">
-          Welcome to Bleep Pizza!
-        </h1>
-        <p className="text-center text-gray-600">
-          Your favorite pizza delivery service.
-        </p>
+      <main className="container mx-auto px-4 py-16 flex flex-col md:flex-row items-center gap-8">
+        <div className="flex-1">
+          <p className="uppercase tracking-[0.2em] text-sm text-red-500">
+            Detroit-style perfection
+          </p>
+        </div>
       </main>
   );
 }
